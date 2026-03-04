c0633332aa49372db81dceba65b67a86f1e95683
Plizzpat <plizzpat.2@gmail.com>
2026-03-04 17:55:31 +0500

    Update sprint_system.lua

--- scripts/player/sprint_system.lua
+++ scripts/player/sprint_system.lua
@@ -1,1 +1,32 @@
+-- Sprint System (client)
+-- Put this LocalScript in StarterPlayerScripts
 
+local UIS = game:GetService("UserInputService")
+local player = game.Players.LocalPlayer
+
+local WALK = 16
+local SPRINT = 24
+
+local humanoid
+
+local function bindCharacter(char)
+	humanoid = char:WaitForChild("Humanoid")
+	humanoid.WalkSpeed = WALK
+end
+
+if player.Character then bindCharacter(player.Character) end
+player.CharacterAdded:Connect(bindCharacter)
+
+UIS.InputBegan:Connect(function(input, gp)
+	if gp or not humanoid then return end
+	if input.KeyCode == Enum.KeyCode.LeftShift then
+		humanoid.WalkSpeed = SPRINT
+	end
+end)
+
+UIS.InputEnded:Connect(function(input, gp)
+	if gp or not humanoid then return end
+	if input.KeyCode == Enum.KeyCode.LeftShift then
+		humanoid.WalkSpeed = WALK
+	end
+end)
